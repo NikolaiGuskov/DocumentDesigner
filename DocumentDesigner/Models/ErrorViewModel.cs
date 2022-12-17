f9644d74d4b546ab64ed150961017337d7205e74
Николай Гуськов <NikolaiPokrovskiy@yandex.ru>
2022-12-17 17:35:00 +0300

    Добавьте файлы проекта.

---
+++ DocumentDesigner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DocumentDesigner.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
