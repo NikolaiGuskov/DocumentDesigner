fb93db37f8438ecebb4d8a218bad15293edfa3b7
Николай Гуськов <NikolaiPokrovskiy@yandex.ru>
2022-12-17 21:39:39 +0300

    Добавил представления для авторизации и регистрации

---
+++ DocumentDesigner/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocumentDesigner.WebApi.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "Введите свое имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите свою фамилию")]
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "Не указан Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли несовпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
